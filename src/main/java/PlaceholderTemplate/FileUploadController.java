03140e03aca5edce8627ca00e4f70e27012b76cc
p.kucher <p.kucher@corp.mail.ru>
2021-02-13 01:36:36 +0700

    ZAD-4

--- src/main/java/PlaceholderTemplate/FileUploadController.java
+++ src/main/java/PlaceholderTemplate/FileUploadController.java
@@ -12,9 +12,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
