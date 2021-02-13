e34183df6ee65d8ec99246125eb36b379a13b1d4
OlyaLolya <oleka211999@mail.ru>
2021-02-13 13:36:59 +0300

    format check from enum

--- src/main/java/PlaceholderTemplate/Services/StorageService.java
+++ src/main/java/PlaceholderTemplate/Services/StorageService.java
@@ -5,11 +5,14 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import PlaceholderTemplate.FileUtils.FileFormat;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import java.util.Arrays;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,11 +23,12 @@ public class StorageService {
     private String path;
     private static final Logger log = LoggerFactory.getLogger(StorageService.class);
 
+
     public void uploadFile(MultipartFile file) {
         if (file.isEmpty()) {
             throw new StorageException("Failed to store empty file");
         }
-        if(!isDoc(file)){
+        if(!IsValidDocumentFormat(file)){
             throw new StorageException("Wrong format!");
         }
         try {
@@ -35,16 +39,27 @@ public class StorageService {
             log.error("Failed to store file:{}",file.getName(),e);
         }
     }
-    public boolean isDoc(MultipartFile file) {
-        String fileFormat = "";
+    public boolean IsValidDocumentFormat(MultipartFile file) {
+        String formatsStr;
         String fileName = file.getOriginalFilename();
+        String formatOfFile = "";
+        formatsStr = Arrays.toString(FileFormat.values());
+        String[] formats = formatsStr.replace("[", "").replace("]", "").
+                split(". ");
+
+        boolean isValid = false;
+
         try {
             if (fileName.contains(".")) {
-                fileFormat = fileName.substring(fileName.lastIndexOf(".") + 1);
+                formatOfFile = fileName.substring(fileName.lastIndexOf(".") + 1);
             }
-        } catch (NullPointerException e){
-            log.error("Missing file extension:{}", fileFormat, e);
+        } catch (NullPointerException e) {
+            log.error("Missing file extension:{}", formatOfFile, e);
         }
-        return fileFormat.equals("doc");
+
+        for (String format : formats) {
+            if (formatOfFile.equals(format.toLowerCase())) isValid = true;
+        }
+        return isValid;
     }
 }
